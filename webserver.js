0d45138e91e538a9ccf2e1c4234f590f0630f142
pip10127 <57880328+pip10127@users.noreply.github.com>
2020-10-05 13:51:33 -0600

    Update webserver.js

--- webserver.js
+++ webserver.js
@@ -1,12 +1,4 @@
-var http = require('http'),
-    fs = require('fs');
-fs.readFile('/var/nodeFiles/www/index.html', function (err, html) {
-    if (err) {
-        throw err; 
-    }       
-    http.createServer(function(request, response) {  
-        response.writeHeader(200, {"Content-Type": "text/html"});  
-        response.write(html);  
-        response.end();  
-    }).listen(80);
-});
+var express = require('express'),
+app = express(); 
+app.use('/', express.static(index.html+ '/'));
+app.listen(8080);
